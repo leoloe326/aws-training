2cc75a50a34542f91f035539fe3271a0bf3e6266
Nan Dun <nan.dun@acm.org>
2017-02-19 12:23:01 -0800

    Fix userdata.sh

--- cluster/userdata.sh
+++ cluster/userdata.sh
@@ -4,6 +4,6 @@ SETUP_SCRIPT=dun-us-west-2/aws/setup.sh
 SETUP_USER=ec2-user
 
 aws s3 cp s3://${SETUP_SCRIPT} /home/${SETUP_USER}/setup.sh
-chown ec2-user:ec2-user /home/${SETUP_USER}/setup.sh
+chown ${SETUP_USER}:${SETUP_USER} /home/${SETUP_USER}/setup.sh
 chmod 700 /home/${SETUP_USER}/setup.sh
-su - ec2-user -c "/home/${SETUP_USER}/setup.sh"
+su - ${SETUP_USER} -c "/home/${SETUP_USER}/setup.sh"
