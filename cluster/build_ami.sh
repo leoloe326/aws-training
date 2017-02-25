28687a0d7e23d343e8ae15422a3738304e3cdebc
Nan Dun <nan.dun@acm.org>
2017-02-25 14:38:22 -0800

    Fix AMI build

--- cluster/build_ami.sh
+++ cluster/build_ami.sh
@@ -4,10 +4,18 @@
 
 set -e
 
-AMI_SPEC=${1:-"amzn-ami.json ecs-ami.json"}
+AMI_SPEC=${1:-"ami.json"}
 
-for ami in ${AMI_SPEC}; do
-	echo "Build customized AMI from ${ami}..."
-	packer validate ./${ami}
-	packer build -color=false ${ami}
-done
+AMI="ami-f173cc91"
+NAME="BitTiger-Amazon-Linux-AMI-2017.01.1-x86_64-HVM-SSD"
+ARGS="-var source_ami_id=${AMI} -var ami_name_prefix=${NAME} ${AMI_SPEC}"
+echo "Build customized Amazon Linux AMI from ${AMI} ..."
+packer validate ${ARGS}
+packer build ${ARGS}
+
+AMI="ami-022b9262"
+NAME="BitTiger-Amazon-ECS-AMI-2016.09.f-x86_64-HVM-GP2"
+ARGS="-var source_ami_id=${AMI} -var ami_name_prefix=${NAME} ${AMI_SPEC}"
+echo "Build customized Amazon Linux AMI from ${AMI} ..."
+packer validate ${ARGS}
+packer build ${ARGS}
